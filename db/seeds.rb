c4988ad6bde89846da2e3493b96799752f73446e
AntoineHaller <antoine.haller@ynov.com>
2021-01-29 09:33:34 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+####### USERS ########
+user1 = User.create(first_name:'Emmanuel', last_name:'President' email:'emmanuel@mail.com', password:'123');
+
+####### APPARTEMENTS ########
+appart1 = Appartements.create(name:"test1", price:1000, description:"description blabla", address:"32 rue du moulin", postcode:77324, city:"Moscou", user_id:user1);
+
+####### MESSAGES ########
+Messages.create(contenu:"test message", user_id:user1, appartement_id:appart1);
